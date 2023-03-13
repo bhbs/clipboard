40815ad1974210ae2498da13c190fbf30b422211
bhbs <bhbstar.me@gmail.com>
2023-03-13 22:49:02 +0900

    fix: remove state

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { ChangeEvent, useCallback, useEffect, useState } from "react";
+import { ChangeEvent, useCallback } from "react";
 import { useRecoilState } from "recoil";
 import { textState } from "./atom";
 import styles from "./App.module.css";
 
 function App() {
   const [text, setText] = useRecoilState(textState);
-  const [state, setState] = useState<boolean>(false);
 
   const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const target = e.target;
     const value = target.value;
     setText(value);
-    setState(false);
   }, []);
 
   const handleSlide = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const target = e.target;
     const value = target.value;
     if (Number(value) > 90) {
-      navigator.clipboard
-        .writeText(text)
-        .then(() => setState(true))
-        .catch(() => setState(false));
+      navigator.clipboard.writeText(text).then(() => {
+        window.close();
+      });
     }
   }, []);
 
@@ -31,16 +28,12 @@ function App() {
         <input type="text" value={text} onChange={handleChange} />
       </p>
       <p>
-        {state ? (
-          "✅"
-        ) : (
-          <input
-            type="range"
-            defaultValue="0"
-            className={styles.confirm}
-            onChange={handleSlide}
-          />
-        )}
+        <input
+          type="range"
+          defaultValue="0"
+          className={styles.confirm}
+          onChange={handleSlide}
+        />
       </p>
     </div>
   );
